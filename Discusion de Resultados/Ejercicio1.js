4a6a13dc7a1b1cef325301c43a13ce61847950be
arisolgo <arielsolano39@gmail.com>
2020-06-29 11:53:05 -0400

    tarea5 completa

---
+++ Discusion de Resultados/Ejercicio1.js
@@ -0,0 +1,4 @@
+document.write("<h1> Heading </h1>");
+document.write("<div class ='dLeft'><p> i never am really satisfied that i understand... </p></div>");
+document.write("<div class = 'dRight'><p> in almost every computation a great variety... </p></div>");
+document.write("<footer> Footer </footer>");
